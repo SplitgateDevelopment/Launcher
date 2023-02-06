41ce6cd8756b1caeba92b428ef35285ef719e5f6
PADO <62028267+47PADO47@users.noreply.github.com>
2023-02-06 21:49:24 +0100

    ✨ Create Hook.h class

---
+++ Internal/Hook.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "Logger.h"
+#include "Engine.h"
+#include <format>
+#include "Features.h"
+
+class Hook {
+
+public:
+	HHOOK g_hook;
+	Logger* logger = new Logger({
+		FALSE,
+	});
+	Features* features = new Features();
+	void** VTABLE;
+
+	bool Init() {
+		if (!EngineInit()) {
+			logger->log("ERROR", "No engine init");
+			return FALSE;
+		};
+
+		UWorld* World = *(UWorld**)(WRLD);
+		if (!World) {
+			logger->log("ERROR", "No World");
+			return FALSE;
+		};
+
+		UGameInstance* OwningGameInstance = World->OwningGameInstance;
+		if (!OwningGameInstance) {
+			logger->log("ERROR", "No owning game instance");
+			return FALSE;
+		};
+
+		TArray<UPlayer*>LocalPlayers = OwningGameInstance->LocalPlayers;
+
+		UPlayer* LocalPlayer = LocalPlayers[0];
+		if (!LocalPlayer) {
+			logger->log("ERROR", "No LocalPlayer");
+			return FALSE;
+		};
+
+		UGameViewportClient* ViewPortClient = LocalPlayer->ViewportClient;
+		if (!ViewPortClient) {
+			logger->log("ERROR", "No UGameViewportClient");
+			return FALSE;
+		};
+
+		void** ViewPortClientVTable = ViewPortClient->VFTable;
+		if (!ViewPortClientVTable) {
+			logger->log("ERROR", "No ViewPortClientVTable");
+			return FALSE;
+		};
+
+		VTABLE = ViewPortClientVTable;
+		return TRUE;
+	}
+
+	void swapRender(void** ViewPortClientVTable, void (*NPostRender)(UGameViewportClient* UGameViewportClient, Canvas* canvas)) {
+
+		DWORD protecc;
+		VirtualProtect(&ViewPortClientVTable[100], 8, PAGE_EXECUTE_READWRITE, &protecc);
+		ViewPortClientVTable[100] = NPostRender;
+		VirtualProtect(&ViewPortClientVTable[100], 8, protecc, 0);
+
+		logger->log("INFO", "Swapped PostRender functions");
+	}
+};
