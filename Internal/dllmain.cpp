c761eb9a0008876b2b6372d491ff79572391b277
PADO <62028267+47PADO47@users.noreply.github.com>
2023-02-06 17:00:44 +0100

    ✨ Delete hook on GetAsyncKeyState(VK_DELETE)

--- Internal/dllmain.cpp
+++ Internal/dllmain.cpp
@@ -6,9 +6,20 @@
 #include "Engine.h"
 #include "logger.h"
 
+HHOOK g_hook;
 Logger* logger = new Logger({
     FALSE,
 });
+
+void swapRender(void** ViewPortClientVTable, void (*NPostRender)(UGameViewportClient* UGameViewportClient, Canvas* canvas)) {
+
+    DWORD protecc;
+    VirtualProtect(&ViewPortClientVTable[100], 8, PAGE_EXECUTE_READWRITE, &protecc);
+    ViewPortClientVTable[100] = NPostRender;
+    VirtualProtect(&ViewPortClientVTable[100], 8, protecc, 0);
+
+    logger->log("INFO", "Swapped PostRender functions");
+}
 
 void PostRender(UGameViewportClient* UGameViewportClient, Canvas* canvas)
 {
@@ -31,11 +42,16 @@ void PostRender(UGameViewportClient* UGameViewportClient, Canvas* canvas)
 		
 		if (GetAsyncKeyState(VK_INSERT) & 1) {
 			logger->log("INFO", "Loading map");
-			PlayerController->SwitchLevel(L"Simulation_Alpha?gameMode=SHOTSNIP");
+			PlayerController->SwitchLevel(L"Simulation_Alpha?Name=SplitgateDevelopment&gameMode=SHOTSNIP");
 		};
 
-        if (GetAsyncKeyState(VK_DELETE) & 1) {
-            float newFov = 120;
+        if (GetAsyncKeyState(VK_DELETE) && 1) {
+            swapRender(LocalPlayer->ViewportClient->VFTable, OPostRender);
+            UnhookWindowsHookEx(g_hook);
+        }
+
+        if (GetAsyncKeyState(VK_RCONTROL) & 1) {
+            float newFov = 140;
             logger->log("INFO", "Changing fov");
 
             PlayerController->FOV(newFov);
@@ -52,19 +68,19 @@ __declspec(dllexport) LRESULT CALLBACK SplitgateCallBack(int code, WPARAM wparam
 
     if (!EngineInit()) {
         logger->log("ERROR", "No engine init");
-        return CallNextHookEx(NULL, code, wparam, HCBT_CREATEWND);
+        return CallNextHookEx(g_hook, code, wparam, HCBT_CREATEWND);
     };
 
     UWorld* World = *(UWorld**)(WRLD);
     if (!World) {
         logger->log("ERROR", "No World");
-        return CallNextHookEx(NULL, code, wparam, HCBT_CREATEWND);
+        return CallNextHookEx(g_hook, code, wparam, HCBT_CREATEWND);
     };
 
     UGameInstance* OwningGameInstance = World->OwningGameInstance;
     if (!OwningGameInstance) {
         logger->log("ERROR", "No owning game instance");
-        return CallNextHookEx(NULL, code, wparam, HCBT_CREATEWND);
+        return CallNextHookEx(g_hook, code, wparam, HCBT_CREATEWND);
     };
 
     TArray<UPlayer*>LocalPlayers = OwningGameInstance->LocalPlayers;
@@ -72,29 +88,26 @@ __declspec(dllexport) LRESULT CALLBACK SplitgateCallBack(int code, WPARAM wparam
     UPlayer* LocalPlayer = LocalPlayers[0];
     if (!LocalPlayer) {
         logger->log("ERROR", "No LocalPlayer");
-        return CallNextHookEx(NULL, code, wparam, HCBT_CREATEWND);
+        return CallNextHookEx(g_hook, code, wparam, HCBT_CREATEWND);
     };
 
     UGameViewportClient* ViewPortClient = LocalPlayer->ViewportClient;
     if (!ViewPortClient) {
         logger->log("ERROR", "No UGameViewportClient");
-        return CallNextHookEx(NULL, code, wparam, HCBT_CREATEWND);
+        return CallNextHookEx(g_hook, code, wparam, HCBT_CREATEWND);
     };
 
     void** ViewPortClientVTable = ViewPortClient->VFTable;
     if (!ViewPortClientVTable) {
         logger->log("ERROR", "No ViewPortClientVTable");
-        return CallNextHookEx(NULL, code, wparam, HCBT_CREATEWND);
+        return CallNextHookEx(g_hook, code, wparam, HCBT_CREATEWND);
     };
 
-    DWORD protecc;
-    VirtualProtect(&ViewPortClientVTable[100], 8, PAGE_EXECUTE_READWRITE, &protecc);
     OPostRender = reinterpret_cast<decltype(OPostRender)>(ViewPortClientVTable[100]);
-    ViewPortClientVTable[100] = &PostRender;
-    VirtualProtect(&ViewPortClientVTable[100], 8, protecc, 0);
+    swapRender(ViewPortClientVTable, PostRender);
 
     logger->log("SUCCESS", "Injected");
     logger->log("INFO", std::format("Base Address: 0x{:x}\n", (uintptr_t)GetModuleHandleW(0)).c_str());
 
-    return CallNextHookEx(NULL, code, wparam, lparam);
+    return CallNextHookEx(g_hook, code, wparam, lparam);
 }
