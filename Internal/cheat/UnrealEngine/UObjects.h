390595937b014b5696af43f4eca3faf68684e366
PADO <62028267+47PADO47@users.noreply.github.com>
2023-02-18 16:49:54 +0100

    ♻️ Create UObjects.h namespace

---
+++ Internal/cheat/UnrealEngine/UObjects.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Engine.h"
+
+namespace UObjects {
+	UObject* SwitchLevelUFunc;
+	UObject* FOVUFunc;
+	UObject* SetNameUFunc;
+	UObject* K2_DrawLineUFunc;
+	UObject* K2_DrawTextUFunc;
+	UObject* Font;
+	UObject* GetSaveGameUFUnc;
+	UObject* K2_GetPawnUFunc;
+}
