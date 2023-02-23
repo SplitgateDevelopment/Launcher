aa454a81744c8b4123fb26528048b20c6b1c4765
PADO <62028267+47PADO47@users.noreply.github.com>
2023-02-23 19:46:14 +0100

    🩹 Disable ProcessEvent hooking

--- Internal/dllmain.cpp
+++ Internal/dllmain.cpp
@@ -42,8 +42,8 @@ void PostRender(UGameViewportClient* UGameViewportClient, Canvas* canvas)
 	return hook->OriginalPostRender(UGameViewportClient, canvas);
 }
 
-void ProcessEvent(UObject* pe, UObject* fn, void* params) {
-	return hook->OriginalProcessEvent(pe, fn, params);
+void ProcessEvent(UObject* Class, UObject* Function, void* params) {
+	return hook->OriginalProcessEvent(Class, Function, params);
 };
 
 __declspec(dllexport) LRESULT CALLBACK SplitgateCallBack(int code, WPARAM wparam, LPARAM lparam) {
@@ -53,7 +53,7 @@ __declspec(dllexport) LRESULT CALLBACK SplitgateCallBack(int code, WPARAM wparam
 	if (!hook->Init()) return CallNextHookEx(hook->g_hook, code, wparam, HCBT_CREATEWND);
 
 	hook->OriginalPostRender = reinterpret_cast<decltype(hook->OriginalPostRender)>(hook->setHook(hook->PostRenderVTable, hook->PostRenderIndex, &PostRender));
-	hook->OriginalProcessEvent = reinterpret_cast<decltype(hook->OriginalProcessEvent)>(hook->setHook(hook->ProcessEventVTable, hook->ProcessEventIndex, &ProcessEvent));
+	//hook->OriginalProcessEvent = reinterpret_cast<decltype(hook->OriginalProcessEvent)>(hook->setHook(hook->ProcessEventVTable, hook->ProcessEventIndex, &ProcessEvent));
 
 	hook->logger->log("SUCCESS", "Injected");
 	hook->logger->log("INFO", std::format("Base Address: [0x{:x}]", (uintptr_t)GetModuleHandleW(0)).c_str());
