171393a7bd818b6ef69b64159a95f2012122edf0
PADO <62028267+47PADO47@users.noreply.github.com>
2023-02-24 20:32:58 +0100

    🚧 Unhooking

--- Internal/dllmain.cpp
+++ Internal/dllmain.cpp
@@ -12,6 +12,8 @@ Menu* menu = new Menu();
 void PostRender(UGameViewportClient* UGameViewportClient, Canvas* canvas)
 {
 	do {
+		if (Settings::Unload) return hook->UnHook();
+
 		ZeroGUI::SetupCanvas(canvas);
 		menu->Tick();
 		menu->DrawWatermark(canvas);
@@ -30,12 +32,6 @@ void PostRender(UGameViewportClient* UGameViewportClient, Canvas* canvas)
 		APlayerController* PlayerController = LocalPlayer->PlayerController;
 		if (!PlayerController) break;
 
-		/*if(Settings::UnHook) {
-			hook->swapRender(LocalPlayer->ViewportClient->VFTable, OPostRender);
-			OPostRender(UGameViewportClient, canvas);
-			UnhookWindowsHookEx(hook->g_hook);
-		}*/
-
 		hook->features->handle(PlayerController);
 	} while (false);
 
@@ -52,8 +48,8 @@ __declspec(dllexport) LRESULT CALLBACK SplitgateCallBack(int code, WPARAM wparam
 
 	if (!hook->Init()) return CallNextHookEx(hook->g_hook, code, wparam, HCBT_CREATEWND);
 
-	hook->OriginalPostRender = reinterpret_cast<decltype(hook->OriginalPostRender)>(hook->setHook(hook->PostRenderVTable, hook->PostRenderIndex, &PostRender));
-	//hook->OriginalProcessEvent = reinterpret_cast<decltype(hook->OriginalProcessEvent)>(hook->setHook(hook->ProcessEventVTable, hook->ProcessEventIndex, &ProcessEvent));
+	hook->OriginalPostRender = reinterpret_cast<decltype(hook->OriginalPostRender)>(hook->SetHook(hook->PostRenderVTable, hook->PostRenderIndex, &PostRender));
+	//hook->OriginalProcessEvent = reinterpret_cast<decltype(hook->OriginalProcessEvent)>(hook->SetHook(hook->ProcessEventVTable, hook->ProcessEventIndex, &ProcessEvent));
 
 	hook->logger->log("SUCCESS", "Injected");
 	hook->logger->log("INFO", std::format("Base Address: [0x{:x}]", (uintptr_t)GetModuleHandleW(0)).c_str());
