d6eeeab261e257be646199a9c748a75e7af1bc65
PADO <62028267+47PADO47@users.noreply.github.com>
2023-03-10 23:35:47 +0100

    ✨ Add NoClip and SpinBot

--- Internal/cheat/Features.h
+++ Internal/cheat/Features.h
@@ -13,6 +13,8 @@ public:
 
 	void handle(APlayerController* PlayerController) {
 		do {
+			//if (Settings::DestroyConsole) logger->DestroyConsole();
+			
 			PlayerController->SetName(L"SplitgateDevelopement");
 			PlayerController->FOV(Settings::FOV);
 
@@ -29,6 +31,14 @@ public:
 			SetState("In game");
 
 			APawn* Player = PlayerController->PlayerState->PawnPrivate;
+			USkeletalMeshComponent* Mesh = Player->Mesh;
+
+			collision = false;
+			if (GetAsyncKeyState(Settings::NoClip)) collision = !collision;
+
+			if (collision && Player->GetActorEnableCollision()) Player->SetActorEnableCollision(false);
+			else if (!collision && !Player->GetActorEnableCollision()) Player->SetActorEnableCollision(true);
+			
 
 			if (Settings::GodMode && Player->Health != 0) {
 				float health = 999999;
@@ -37,12 +47,22 @@ public:
 
 				//Instigator->healthRechargeDelay = 0.1f;
 			};
+
+			if (Settings::SpinBot && Mesh) {
+				if (spin_yaw > 360.f) spin_yaw = 0.f;
+
+				Mesh->K2_SetRelativeRotation(FRotator(0.f, spin_yaw, 0.f), true, false);
+				spin_yaw += 20.f;
+			};
 		} while (false);
 
 		return;
 	};
 
 private:
+	bool collision = false;
+	float spin_yaw = 0.f;
+
 	bool isIngame(APlayerController* PlayerController) {
 		return (PlayerController->AcknowledgedPawn);
 	}
