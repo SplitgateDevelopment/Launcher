2e94c93ec9bcd62596879818a332fa22e73b4c72
PADO <62028267+47PADO47@users.noreply.github.com>
2023-03-10 23:38:55 +0100

    ♻ Logger.h cleanup

--- Internal/utils/Logger.h
+++ Internal/utils/Logger.h
@@ -21,6 +21,10 @@ public:
         CreateConsole();
     }
 
+    ~Logger() {
+        DestroyConsole();
+    };
+
     void log(const string title, const string message)
     {
         time_t now = time(0);
@@ -40,79 +44,23 @@ public:
 
 
     void DestroyConsole() {
+        log("INFO", "Destroying console");
         fclose(file);
-        FreeConsole();
+        if (FreeConsole())
+        {
+            fflush(stdout);
+            fflush(stderr);
+        };
     }
 
 private:
-    HANDLE g_hChildStd_IN_Rd = NULL;
-    HANDLE g_hChildStd_IN_Wr = NULL;
-    HANDLE g_hChildStd_OUT_Rd = NULL;
-    HANDLE g_hChildStd_OUT_Wr = NULL;
     FILE* file = new FILE();
 
     void CreateConsole()
     {
-        SECURITY_ATTRIBUTES saAttr;
-        saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
-        saAttr.bInheritHandle = TRUE;
-        saAttr.lpSecurityDescriptor = NULL;
-
-        // Create a pipe for the child process's STDOUT.
-        if (!CreatePipe(&g_hChildStd_OUT_Rd, &g_hChildStd_OUT_Wr, &saAttr, 0))
-            return;
-
-        // Ensure the read handle to the pipe for STDOUT is not inherited.
-        if (!SetHandleInformation(g_hChildStd_OUT_Rd, HANDLE_FLAG_INHERIT, 0))
-            return;
-
-        // Create a pipe for the child process's STDIN.
-        if (!CreatePipe(&g_hChildStd_IN_Rd, &g_hChildStd_IN_Wr, &saAttr, 0))
-            return;
-
-        // Ensure the write handle to the pipe for STDIN is not inherited.
-        if (!SetHandleInformation(g_hChildStd_IN_Wr, HANDLE_FLAG_INHERIT, 0))
-            return;
-
-        // Save the handle to the current STDOUT.
-        HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
-
-        // Redirect unbuffered STDOUT to the pipe.
-        int fd = _open_osfhandle((intptr_t)g_hChildStd_OUT_Wr, _O_TEXT);
-        if (fd == -1)
-            return;
-        FILE* pf = _fdopen(fd, "w");
-        *stdout = *pf;
-        setvbuf(stdout, NULL, _IONBF, 0);
-
-        // Save the handle to the current STDIN.
-        HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-
-        // Redirect unbuffered STDIN to the pipe.
-        fd = _open_osfhandle((intptr_t)g_hChildStd_IN_Rd, _O_TEXT);
-        if (fd == -1)
-            return;
-        pf = _fdopen(fd, "r");
-        *stdin = *pf;
-        setvbuf(stdin, NULL, _IONBF, 0);
-
-        // Redirect the STDOUT handle to the console window.
-        if (!SetStdHandle(STD_OUTPUT_HANDLE, g_hChildStd_OUT_Wr))
-            return;
-
-        // Redirect the STDIN handle to the console window.
-        if (!SetStdHandle(STD_INPUT_HANDLE, g_hChildStd_IN_Rd))
-            return;
-
-        // Create the console window.
-        if (!AllocConsole())
-        {
-            // If the AllocConsole function fails, restore the original handle.
-            SetStdHandle(STD_OUTPUT_HANDLE, hStdout);
-            SetStdHandle(STD_INPUT_HANDLE, hStdin);
-            return;
-        };
-
+        if (!AllocConsole()) return;
+        freopen_s(&file, "CONIN$", "r", stdin);
+        freopen_s(&file, "CONOUT$", "w", stderr);
         freopen_s(&file, "CONOUT$", "w", stdout);
     };
 
