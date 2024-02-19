ab20a773abe15dd2d78e2eee5eda5eee0307d6dd
PADO <62028267+47PADO47@users.noreply.github.com>
2024-02-19 16:04:38 +0100

    ♻ Use ZeroGUI::canvas to DrawWatermark

--- Internal/menu/Menu.h
+++ Internal/menu/Menu.h
@@ -63,16 +63,18 @@ public:
 				break;
 			};
 		}
+
 		ZeroGUI::Render();
+		DrawWatermark();
 	};
 
-	void DrawWatermark(Canvas* canvas) {
-		if (!Settings.MENU.ShowWatermark) return;
+	void DrawWatermark() {
+		if (!Settings.MENU.ShowWatermark || !ZeroGUI::canvas) return;
 
 		std::string str = Settings.MENU.Watermark;
 		LPCWSTR watermark = std::wstring(str.begin(), str.end()).c_str();
 
-		canvas->K2_DrawText(watermark, {15.f, 15.f}, {1.f, 1.f}, ZeroGUI::Colors::MainColor, 1.f, {0.f, 0.f, 0.f, 0.f}, {0.f, 0.f}, false, false, true, ZeroGUI::Colors::MainColor);
+		ZeroGUI::canvas->K2_DrawText(watermark, {15.f, 15.f}, {1.f, 1.f}, ZeroGUI::Colors::MainColor, 1.f, {0.f, 0.f, 0.f, 0.f}, {0.f, 0.f}, false, false, true, ZeroGUI::Colors::MainColor);
 		return;
 	};;
 };
