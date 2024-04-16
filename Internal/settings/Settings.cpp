32c4b006708bd6fa1e725ac5a9126ad06f135994
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-16 21:37:00 +0200

    ✨ Create SettingsHelper ::GetAppPath

--- Internal/settings/Settings.cpp
+++ Internal/settings/Settings.cpp
@@ -1,61 +1,69 @@
 #include <Windows.h>
 #include <iostream>
-#include <string>
 #include "Settings.h"
-#include <errno.h>
-#include <stdio.h>
+#include <ShlObj.h>
+#include <fstream>
 
-SETTINGS Settings = { 0 };
+SETTINGS Settings;
 
 namespace SettingsHelper {
+	fs::path filename("splitgate.settings");
+	fs::path GetAppPath() {
+		wchar_t* documentsPath = nullptr;
+		std::string appPath;
 
-	std::string GetPath() {
-		std::string filename = "splitgate.settings";
+		HRESULT result = SHGetKnownFolderPath(FOLDERID_Documents, 0, NULL, &documentsPath);
+		if (SUCCEEDED(result)) {
+			std::wstring documentsPathStr(documentsPath);
 
-		CHAR path[0xFF] = { 0 };
-		GetTempPathA(sizeof(path) / sizeof(path[0]), path);
+			CoTaskMemFree(documentsPath);
 
-		return std::string(path).append(filename);
+			fs::path appPath = fs::path(documentsPathStr) / "SplitgateInternal";
+			fs::create_directories(appPath);
+
+			return appPath;
+		}
+
+		return appPath;
+	}
+
+	std::string GetSettingsFilePath() {
+		return (GetAppPath() / filename).string();
 	}
 
 	VOID Save() {
-		std::string path = GetPath();
-		FILE* file;
-		errno_t error = fopen_s(&file, path.data(), "wb");
+		std::string path = GetSettingsFilePath();
+		std::ofstream file(path, std::ios::out | std::ios::binary);
 
-		if (error == 0) {
-			fwrite(&Settings, sizeof(Settings), 1, file);
-			fclose(file);
-			//free(path.data());
+		if (!file.is_open()) {
+			char errorMsg[256];
+			strerror_s(errorMsg, sizeof(errorMsg), errno);
+			std::cerr << "Failed to open settings file for writing: " << errorMsg << std::endl;
+			return;
 		}
+
+		file.write(reinterpret_cast<char*>(&Settings), sizeof(SETTINGS));
+		file.close();
 	}
 
 	bool Init() {
-		std::string path = GetPath();
-		FILE* file;
-		errno_t error = fopen_s(&file, path.data(), "wb");
+		std::string path = GetSettingsFilePath();
+		std::ifstream file(path, std::ios::in | std::ios::binary);
 
-		if (error == 0) {
-			fseek(file, 0, SEEK_END);
-			auto size = ftell(file);
-
-			if (size == sizeof(Settings)) {
-				fseek(file, 0, SEEK_SET);
-				fread(&Settings, sizeof(Settings), 1, file);
-				fclose(file);
-
-				//free(path.data());
-
-				printf("abc");
-				return true;
-			}
-
-			fclose(file);
+		if (!file.is_open()) {
+			Reset();
+			return false;
 		}
 
-		//free(path.data());
-		Reset();
-		return false;
+		file.read(reinterpret_cast<char*>(&Settings), sizeof(SETTINGS));
+		file.close();
+
+		if (sizeof(Settings) != sizeof(SETTINGS)) {
+			Reset();
+			return false;
+		}
+
+		return true;
 	}
 
 	VOID Reset() {
@@ -65,7 +73,8 @@ namespace SettingsHelper {
 		Settings.MENU.ShowWatermark = true;
 		Settings.MENU.Watermark = "github.com/SplitgateDevelopment/Launcher";
 		Settings.MENU.ShowHotkey = VK_INSERT;
-		Settings.MENU.MenuPosition = {};
+		Settings.MENU.MenuPosition = { 0.f, 0.f };
+		Settings.MENU.MenuColor = { 255.0f, 0.0f, 0.0f, 1.0f };
 
 		Settings.EXPLOITS.FOV = 80;
 		Settings.EXPLOITS.GodMode = false;
@@ -73,12 +82,20 @@ namespace SettingsHelper {
 		Settings.EXPLOITS.NoClip = VK_OEM_PLUS;
 
 		Settings.MISC.LoadIntoMap = false;
-		Settings.MISC.Unload = false;
-		Settings.MISC.DestroyConsole = false;
+		Settings.MISC.ShowConsole = true;
 		Settings.MISC.PlayerName = "SplitgateDevelopment";
 		Settings.MISC.DiscordAppID = "1078744504066117703";
 		Settings.MISC.UserScriptsEnabled = false;
+		Settings.MISC.LogProcessEvent = false;
 
 		Save();
+	}
+
+	VOID Delete() {
+		std::string path = GetSettingsFilePath();
+
+		if (fs::exists(path)) {
+			fs::remove(path);
+		};
 	}
 }
