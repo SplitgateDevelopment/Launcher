b40539d53483408311fb5793d4c953504d294911
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-16 22:22:39 +0200

    ✨ Create namespace ProcessEvent

--- Internal/hook/functions/ProcessEvent.h
+++ Internal/hook/functions/ProcessEvent.h
@@ -1,8 +1,55 @@
 #pragma once
 
 #include "../../ue/Engine.h"
-#include "../Hook.h"
+#include "../../settings/Settings.h"
+#include "../../utils/ExceptionHandler.h"
 
-void ProcessEvent(UObject* Class, UObject* Function, void* params) {
-	return Hook::OriginalProcessEvent(Class, Function, params);
-};
+namespace ProcessEvent {
+	void** VTable;
+	void (*Original)(UObject*, UFunction*, void*) = nullptr;
+	int Index = 68;
+
+	void HookedProcessEvent(UObject* Class, UFunction* Function, void* Params) {
+		static UObject* ReceiveShutdown = ObjObjects->FindObject("Function Engine.GameInstance.ReceiveShutdown");
+
+		if (Settings.MISC.LogProcessEvent) {
+
+			std::string className = Class->GetName().c_str();
+			std::string functionName = Function->GetFullName();
+
+			if (className != "DebugCanvasObject") {
+				Logger::Log("INFO", std::format("Caller [{}] Function [{}]", className, functionName));
+			}
+		}
+
+		if (Function == ReceiveShutdown) {
+			Logger::Log("INFO", "Received shutdown");
+			ExceptionHandler::Disable();
+			Logger::DestroyConsole();
+			//Hook::UnHook();
+		};
+
+		/* 
+		TODO: Intercept and modify widget IsInputActionEnabled function
+		static UObject* IsInputActionEnabled = ObjObjects->FindObject("Function PortalWars.PortalWarsUserWidget.IsInputActionEnabled");
+		//Pointer comparison is faster
+		if (Function == IsInputActionEnabled) {
+			Logger::Log("INFO", "IsInputActionEnabled intercepted");
+
+			struct IsInputActionEnabledStruct {
+				struct FGameplayTag& ActionTag;
+				bool& InIsEnabled;
+			} *IsInputActionEnabledParams;
+			IsInputActionEnabledParams = reinterpret_cast<IsInputActionEnabledStruct*>(Params);
+
+			std::cout << IsInputActionEnabledParams << std::endl;
+			
+			IsInputActionEnabledParams->InIsEnabled = true;
+
+			Logger::Log("INFO", "IsInputActionEnabled modified");
+			return;
+		}*/
+
+		return Original(Class, Function, Params);
+	};
+}
