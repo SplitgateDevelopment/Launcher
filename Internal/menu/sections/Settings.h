8feb9cc62d140d7e3e14a2eeac5033469c650c6e
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-16 21:53:26 +0200

    💄 Update Settings Section

--- Internal/menu/sections/Settings.h
+++ Internal/menu/sections/Settings.h
@@ -2,6 +2,7 @@
 
 #include "../../gui/ZeroGUI.h"
 #include "../../settings/Settings.h"
+#include "../../hook/Hook.h"
 
 namespace Menu {
 	namespace Sections {
@@ -20,13 +21,20 @@ namespace Menu {
 			ZeroGUI::SameLine();
 			ZeroGUI::Hotkey("Menu Hotkey", FVector2D{ 110, 25 }, &Settings.MENU.ShowHotkey);
 
-			if (ZeroGUI::Button("Detach Console", FVector2D{ 110, 25 })) Settings.MISC.DestroyConsole = true;
+			if (ZeroGUI::Button("Toggle Console", FVector2D{ 110, 25 })) {
+				Settings.MISC.ShowConsole = !Settings.MISC.ShowConsole;
+				Logger::SetConsoleVisibility(Settings.MISC.ShowConsole);
+			};
 			ZeroGUI::SameLine();
-			if (ZeroGUI::Button("Unload", FVector2D{ 110, 25 })) Settings.MISC.Unload = true;
+			if (ZeroGUI::Button("Unload", FVector2D{ 110, 25 })) {
+				Hook::UnHook();
+			}
 
-			ZeroGUI::ColorPicker("Menu Color", &ZeroGUI::Colors::MainColor);
+			ZeroGUI::ColorPicker("Menu Color", &Settings.MENU.MenuColor);
 			ZeroGUI::SameLine();
 			ZeroGUI::Checkbox("Watermark", &Settings.MENU.ShowWatermark);
+
+			ZeroGUI::Checkbox("Log ProcessEvent", &Settings.MISC.LogProcessEvent);
 		}
 	}
 }
