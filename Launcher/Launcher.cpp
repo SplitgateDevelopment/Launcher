0c39c4cdb7b9996682ed04540b637ee3224808c0
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-16 22:26:30 +0200

    ♻️ Add error text box

--- Launcher/Launcher.cpp
+++ Launcher/Launcher.cpp
@@ -1,62 +1,58 @@
-#include <Windows.h>
 #include "utils/Logger.h"
-
-int stop(int code) {
-	std::cin.get();
-	return code;
-};
+#include <thread>
+#include <chrono>
 
 int main() {
 	SetConsoleTitleA("Splitgate Launcher");
-
-	HMODULE lib = LoadLibraryA("internal.dll");
 	Logger* logger = new Logger();
 
+	HMODULE lib = LoadLibraryA("internal.dll");
 	if (!lib) {
 		logger->error("Failed to load target module!");
-		return stop(-1);
+		logger->errorBox(TEXT("LoadLibraryA"));
+		return logger->stop(-1);
 	}
 	logger->success("Loaded target module!");
 
 	HOOKPROC proc = reinterpret_cast<HOOKPROC>(GetProcAddress(lib, "?SplitgateCallBack@@YA_JH_K_J@Z"));
 	if (!proc) {
 		logger->error("Failed to get exported function address!");
-		return stop(-1);
+		logger->errorBox(TEXT("HOOKPROC"));
+		return logger->stop(-1);
 	}
 	logger->success("Got exported function address!");
 
 	HWND GameWindow = FindWindowA(0, "PortalWars  ");
 	if (!GameWindow) {
 		logger->error("Failed to get game window!");
-		return stop(-1);
+		return logger->stop(-1);
 	}
 	logger->success("Got game window!");
 
 	DWORD ProcessID = 0, ThreadID = GetWindowThreadProcessId(GameWindow, &ProcessID);
 	if (!ThreadID) {
 		logger->error("Failed to get thread id!");
-		return stop(-1);
+		logger->errorBox(TEXT("GetWindowThreadProcessId"));
+		return logger->stop(-1);
 	}
-	logger->success(std::format("Got thread id: {}", ThreadID));
-	logger->success(std::format("Got process id: {}", ProcessID));
+	logger->success(std::format("Thread id: {}", ThreadID));
+	logger->success(std::format("Process id: {}", ProcessID));
 
 	HHOOK hook = SetWindowsHookExW(WH_GETMESSAGE, proc, lib, ThreadID);
 	if (!hook) {
 		logger->error("Failed to place hook");
-		return stop(-1);
+		logger->errorBox(TEXT("SetWindowsHookExW"));
+		return logger->stop(-1);
 	};
 	logger->success("Placed hook!");
 
-	PostThreadMessageW(ThreadID, HCBT_CREATEWND, 0, reinterpret_cast<LPARAM>(hook));
-
+	if (!PostThreadMessageW(ThreadID, HCBT_CREATEWND, 0, reinterpret_cast<LPARAM>(hook))) {
+		logger->error("Failed to post thread message!");
+		logger->errorBox(TEXT("PostThreadMessageW"));
+		return logger->stop(-1);
+	}
 	logger->success("DLL injected into process!");
 
-	while (FindWindowA(0, "PortalWars  ")) {
-		if (GetAsyncKeyState(VK_DELETE)) {
-			logger->info("Unloading");
-			break;
-		}
-	}
-
+	std::this_thread::sleep_for(std::chrono::seconds(2));
 	return 0;
 }
