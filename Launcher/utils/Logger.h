e4ef7c4995c7d942d09e760d847dd3d363cd12cf
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-16 22:24:49 +0200

    🔊 Add errorBox and stop to logger

--- Launcher/utils/Logger.h
+++ Launcher/utils/Logger.h
@@ -1,6 +1,9 @@
+#pragma once
+
 #include <iostream>
 #include <format>
-
+#include <Windows.h>
+#include <strsafe.h>
 
 struct logDTO {
 	std::string text;
@@ -24,6 +27,42 @@ public:
 		return _log({
 			"INFO",
 			}, message);
+	};
+
+	//https://learn.microsoft.com/en-us/windows/win32/debug/retrieving-the-last-error-code
+	void errorBox(LPCTSTR lpszFunction) {
+		LPVOID lpMsgBuf;
+		LPVOID lpDisplayBuf;
+		DWORD dw = GetLastError();
+
+		FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_SYSTEM |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			dw,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&lpMsgBuf,
+			0, NULL);
+
+		// Display the error message and exit the process
+
+		lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+			(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+		StringCchPrintf((LPTSTR)lpDisplayBuf,
+			LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+			TEXT("%s failed with error %d: %s"),
+			lpszFunction, dw, lpMsgBuf);
+		MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK);
+
+		LocalFree(lpMsgBuf);
+		LocalFree(lpDisplayBuf);
+	}
+
+	int stop(int code) {
+		info("Press any key to exit...");
+		std::cin.get();
+		return code;
 	};
 
 private:
