81fc5ed4074aa5a0911c07ff421cead14fea1819
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-16 21:52:01 +0200

    🩹 Convert Watermark to string

--- Internal/menu/sections/Watermark.h
+++ Internal/menu/sections/Watermark.h
@@ -8,7 +8,7 @@ namespace Menu {
 		void Watermark() {
 			if (!Settings.MENU.ShowWatermark || !ZeroGUI::canvas) return;
 
-			std::string str = Settings.MENU.Watermark;
+			std::string str = std::string(Settings.MENU.Watermark);
 			LPCWSTR watermark = std::wstring(str.begin(), str.end()).c_str();
 
 			ZeroGUI::canvas->K2_DrawText(watermark, { 15.f, 15.f }, { 1.f, 1.f }, ZeroGUI::Colors::MainColor, 1.f, { 0.f, 0.f, 0.f, 0.f }, { 0.f, 0.f }, false, false, true, ZeroGUI::Colors::MainColor);
