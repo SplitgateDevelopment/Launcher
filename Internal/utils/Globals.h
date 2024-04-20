3ba167b7bc34bc362da0b1b5d90bc1fb9b591aae
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-20 18:43:19 +0200

    ✨ Add new globals

--- Internal/utils/Globals.h
+++ Internal/utils/Globals.h
@@ -5,13 +5,16 @@
 namespace Globals {
 	UEngine* Engine = 0;
 	UWorld* World = 0;
+	APortalWarsPlayerController* PlayerController = 0;
 	UGameplayStatics* GameplayStatics = 0;
 	UKismetStringLibrary* KismetStringLibrary = 0;
+	UKismetTextLibrary* KismetTextLibrary = 0;
 
 	void Init() {
 		Engine = UEngine::GetEngine();
 		World = UWorld::GetWorld();
 		GameplayStatics = reinterpret_cast<UGameplayStatics*>(UGameplayStatics::StaticClass());
 		KismetStringLibrary = reinterpret_cast<UKismetStringLibrary*>(UKismetStringLibrary::StaticClass());
+		KismetTextLibrary = reinterpret_cast<UKismetTextLibrary*>(UKismetTextLibrary::StaticClass());
 	}
 }
