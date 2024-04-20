32630f15ff42055a6f6b9914e8eb265d0e7f8320
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-20 18:11:40 +0200

    🔊 Update ProcessEvent logging

--- Internal/hook/functions/ProcessEvent.h
+++ Internal/hook/functions/ProcessEvent.h
@@ -9,18 +9,59 @@ namespace ProcessEvent {
 	void (*Original)(UObject*, UFunction*, void*) = nullptr;
 	int Index = 68;
 
-	void HookedProcessEvent(UObject* Class, UFunction* Function, void* Params) {
-		static UObject* ReceiveShutdown = ObjObjects->FindObject("Function Engine.GameInstance.ReceiveShutdown");
+	static std::vector<std::string> filteredWords
+	{
+		"SetName",
+		"ServerChangeName",
+		"FOV",
+		"UMG",
+		"CameraModifier",
+		"BlueprintUpdateAnimation",
+		"BlueprintPostEvaluateAnimation",
+		"KismetMathLibrary",
+		"KismetTextLibrary",
+		"Construct",
+		"PopulateItems",
+		"WBP",
+		"IsInputActionEnabled",
+		"GetNewVisibility",
+		"Focus",
+		"OnControllerStatusChanged",
+		"GetDataContext",
+		"Hover",
+		"Unhover",
+		"OnText",
+		"OnInitiallyDisplayed",
+		"OnDataContextChanged",
+		"DelegateSignature",
+		"GetActorEnableCollision",
+		"PerformConditionCheck",
+		"LocalizedMessage",
+		"ReadyToEndMatch",
+		"ClientHearAk",
+		"OnActorBump",
+		"Received_Notify",
+	};
 
-		if (Settings.DEBUG.LogProcessEvent) {
+	void LogProcessEvent(UObject* Class, UFunction* Function)
+	{
+		if (!Settings.DEBUG.LogProcessEvent) return;
 
-			std::string className = Class->GetName().c_str();
-			std::string functionName = Function->GetFullName();
+		std::string className = Class->GetName().c_str();
+		std::string functionName = Function->GetFullName();
 
-			if (className != "DebugCanvasObject") {
-				Logger::Log("INFO", std::format("Caller [{}] Function [{}]", className, functionName));
-			}
+		if (className == "DebugCanvasObject") return;
+		for (const auto& word : filteredWords) {
+			if (functionName.find(word) != std::string::npos) return;
 		}
+
+		Logger::Log("INFO", std::format("Caller [{}] Function [{}]", className, functionName));
+	}
+
+	void HookedProcessEvent(UObject* Class, UFunction* Function, void* Params) {
+		LogProcessEvent(Class, Function);
+
+		static UObject* ReceiveShutdown = ObjObjects->FindObject("Function Engine.GameInstance.ReceiveShutdown");
 
 		if (Function == ReceiveShutdown) {
 			Logger::Log("INFO", "Received shutdown");
