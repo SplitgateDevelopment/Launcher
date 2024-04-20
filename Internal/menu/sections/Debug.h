9c80aa75a29fe8f0991c1ea8d8f17946ea5a0f3a
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-20 18:43:32 +0200

    ✨ Add bot summoning

--- Internal/menu/sections/Debug.h
+++ Internal/menu/sections/Debug.h
@@ -9,6 +9,15 @@ namespace Menu {
 			ImGui::ToggleButton("Log ProcessEvent", &Settings.DEBUG.LogProcessEvent);
 			ImGui::ToggleButton("Show demo window", &Settings.DEBUG.ShowDemoWindow);
 			ImGui::ToggleButton("Show style editor", &Settings.DEBUG.ShowStyleEditor);
+			
+			if (ImGui::Button("Summon Bot"))
+			{
+				if (!Globals::PlayerController) return;
+				if (!Globals::PlayerController->IsInGame()) return;
+
+				Globals::PlayerController->SendToConsole(FString("summon PortalWarsBot_BP_C"));
+				Globals::PlayerController->SendChatMessage(FString("Summoned actor"));
+			}
 		}
 	}
 }
