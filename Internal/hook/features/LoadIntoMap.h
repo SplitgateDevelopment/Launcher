871b614f9efde5752288401382db84de6f2842eb
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-21 20:59:40 +0200

    ✨ Create LoadIntoMap Feature

---
+++ Internal/hook/features/LoadIntoMap.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "../Feature.h"
+#include "../../utils/Globals.h"
+
+class LoadIntoMap : public Feature
+{
+public:
+	LoadIntoMap()
+	{
+		Name = "LoadIntoMap";
+		UpdateEnabled();
+
+		Log("Created");
+	};
+
+	void UpdateEnabled()
+	{
+		Enabled = Settings.MISC.LoadIntoMap;
+	};
+
+	bool Check()
+	{
+		UpdateEnabled();
+
+		if (!Initialized) return false;
+
+		if (!Globals::PlayerController) return false;
+		if (Globals::PlayerController->IsInGame()) return false;
+
+		Log(Enabled ? "Enabled" : "Not Enabled");
+		return Enabled;
+	};
+
+	void Init()
+	{
+		Initialized = true;
+		Log("Initialized");
+	};
+
+	void Destroy()
+	{
+	};
+
+	void Run()
+	{
+		//TODO Enable Play Button
+		//UPortalWarsPlayButtonWidget:UPortalWarsButtonWidget:UPortalWarsGenericButton:UPortalWarsUserWidget:UUserWidget:UWidget:UVisual:UObject
+		//bool IsEnabled(); // Function PortalWars.PortalWarsButtonWidget.IsEnabled // (Native|Public|Const) // @ game+0x1664af0
+
+		Logger::Log("INFO", "Loading into map");
+		Globals::PlayerController->SwitchLevel(L"Simulation_Alpha");
+		Settings.MISC.LoadIntoMap = false;
+	};
+};
