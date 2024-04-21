fb2491b1a8edc0dea7f308a3962159ceb4bbb921
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-21 10:47:26 +0200

    ✨ Add GObjects dumping

--- Internal/menu/sections/Debug.h
+++ Internal/menu/sections/Debug.h
@@ -18,6 +18,38 @@ namespace Menu {
 				Globals::PlayerController->SendToConsole(FString("summon PortalWarsBot_BP_C"));
 				Globals::PlayerController->SendChatMessage(FString("Summoned actor"));
 			}
+
+			if (ImGui::Button("Dump GObjects"))
+			{
+				fs::path dumpsDir = SettingsHelper::GetAppPath() / "Dumps";
+				if (!fs::exists(dumpsDir)) fs::create_directories(dumpsDir);
+
+				fs::path filePath = dumpsDir / "GObjects.txt";
+
+				std::ofstream file(filePath, std::ios::out | std::ios::trunc);
+				if (!file.is_open()) {
+					char errorMsg[256];
+					strerror_s(errorMsg, sizeof(errorMsg), errno);
+
+					Logger::Log("ERROR", std::format("Failed to open {} for writing: {}", filePath.string(), errorMsg));
+					return;
+				}
+
+				auto objNum = ObjObjects->NumElements;
+				for (auto i = 0u; i < objNum; i++)
+				{
+					auto Object = ObjObjects->GetObjectPtr(i);
+					if (!Object) continue;
+
+					file << '[' + std::to_string(Object->InternalIndex) + "] " + Object->GetFullName() << '\n';
+				}
+
+				file.close();
+
+				std::string msg = std::format("Dumped {} GObjects to {}", objNum, filePath.string());
+				Logger::Log("SUCCESS", msg);
+				Globals::PlayerController->SendChatMessage(FString(msg));
+			}
 		}
 	}
 }
