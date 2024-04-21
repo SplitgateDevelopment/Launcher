2044f3cfddfe13789c3b8f63510b2593be7adf2e
PADO <62028267+47PADO47@users.noreply.github.com>
2024-04-21 20:58:55 +0200

    ✨ Create Feature class

---
+++ Internal/hook/Feature.h
@@ -0,0 +1,23 @@
+#pragma once
+
+class Feature
+{
+public:
+	bool Enabled = false;
+	bool Initialized = false;
+	std::string Name = "BaseFeature";
+
+	Feature() {};
+
+	virtual void Init() = 0;
+	virtual void UpdateEnabled() = 0;
+	virtual bool Check() = 0;
+	virtual void Destroy() = 0;
+	virtual void Run() = 0;
+
+	void Log(std::string message)
+	{
+		if (!Settings.DEBUG.FeaturesLogging) return;
+		Logger::Log("FEATURE", std::format("[{}] {}", Name, message));
+	};
+};
