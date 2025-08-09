13afe638a0b145c6979498fad68cf634793d0eea
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2025-08-09 22:06:15 +0200

    chore: update imgui config

--- Internal/menu/gui/Config.h
+++ Internal/menu/gui/Config.h
@@ -4,11 +4,16 @@
 namespace GUI {
 	namespace Config {
 		void Init() {
+			auto iniPath = SettingsHelper::GetAppPath("imgui.ini").string();
+			auto logsPath = SettingsHelper::GetAppPath("imgui_log.txt").string();
+
 			ImGuiIO& io = ImGui::GetIO(); (void)io;
 
 			io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 			io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-			io.IniFilename = 0;
+
+			io.IniFilename = iniPath.c_str();
+			io.LogFilename = logsPath.c_str();
 		}
 	}
 }
