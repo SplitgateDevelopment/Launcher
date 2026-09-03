4fcb49279b76f8a2f252ff85c282b046e5ec59a0
47PADO47 <62028267+47PADO47@users.noreply.github.com>
2026-09-03 21:59:52 +0200

    fix: dll main

--- Internal/dllmain.cpp
+++ Internal/dllmain.cpp
@@ -18,7 +18,7 @@ __declspec(dllexport) LRESULT CALLBACK SplitgateCallBack(int code, WPARAM wparam
 	MSG* msg = (MSG*)lparam;
 
 	constexpr UINT WM_SPLITGATE_INIT = WM_APP + 1;
-	if (msg->message != WM_SPLITGATE_INIT || g_initialized)
+	if (msg->message != WM_SPLITGATE_INIT || Hook::g_initialized)
 	{
 		return CallNextHookEx(Hook::g_hook, code, wparam, lparam);
 	}
